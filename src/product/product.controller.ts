998afc9d5ad39675fd2f415b6deca1a58c5bba1a
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-06-13 16:54:47 -0300

    feat: complete products crud for fun

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductEntity } from './product.entity';
+import { v4 as uuid } from 'uuid';
 
 @Controller('/products')
 export class ProductController {
@@ -11,10 +14,53 @@ export class ProductController {
     return this.productService.getProducts();
   }
 
+  @Get('/:id')
+  async getProduct(
+      @Param('id') id: string
+  ) {
+      const product = await this.productService.getProduct(id);
+      return {
+          statusCode: 200,
+          message: 'Product retrieved successfully',
+          product: product
+      }
+  }
+
   @Post()
     async createProduct(
         @Body() body: CreateProductDto  
     ) {
-        return this.productService.createProduct(body);
+      const productEntity = new ProductEntity();
+      productEntity.name = body.name;
+      productEntity.description = body.description;
+      productEntity.price = body.price;
+      productEntity.id =  uuid();
+      productEntity.qtd = body.qtd;
+      productEntity.details = body.details;
+      productEntity.images = body.images;
+      productEntity.category = body.category;
+      productEntity.createdAt = new Date().toISOString();
+      productEntity.updatedAt = new Date().toISOString();
+      this.productService.createProduct(productEntity);
+      return {
+          product: productEntity,
+          message: 'Product created successfully',
+      }
+
+    }
+
+    @Put('/:id')
+    async updateProduct(
+        @Body() body: UpdateProductDto,
+        @Param('id') id: string
+    ) {
+        return this.productService.updateProduct(id, body);
+    }
+
+    @Delete('/:id')
+    async deleteProduct(
+        @Param('id') id: string
+    ) {
+        return this.productService.deleteProduct(id);
     }
 }
---
+++ src/product/product.entity.ts
@@ -0,0 +1,12 @@
+export class ProductEntity {
+    id: string;
+    name: string;
+    price: number;
+    qtd: number;
+    description: string;
+    details: any[];
+    images: any[];
+    category: string;
+    createdAt: string;
+    updatedAt: string;
+}
