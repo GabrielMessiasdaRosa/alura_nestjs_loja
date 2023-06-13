62ae91a266b079cceb8b2961bb553970eb8e6de1
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-06-13 16:14:08 -0300

    feat: implments user entity

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,24 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { UserEntity } from "./user.entity";
 
 @Injectable() // 👈 Injectable decorator is a class decorator that defines a service, a service is a class that contains business logic and is instantiated within a module.
 export class UserService {
-    private users = []; // 👈 users array memory storage
+    private users: UserEntity[] = []; // 👈 users array memory storage
 
-    async createUser(name: string, email: string, password: string) { // 👈 createUser method that accepts name, email, and password
-        const user = { // 👈 user object
-            name,
-            email,
-            password
-        };
+    async createUser(user: UserEntity) { // 👈 createUser method that accepts name, email, and password
         this.users.push(user); // 👈 push user object to users array, this will be replaced with a database call in the next chapter
-        return user; // 👈 return user object
     }
 
     async getUsers() { // 👈 getUsers method
         return this.users; // 👈 return users array
     }
 
-    async emailAlreadyExists(email: string) { // 👈 emailAlreadyExists method
+    async emailAlreadyExists(email: UserEntity["email"]) { // 👈 emailAlreadyExists method
         return this.users.some(user => user.email === email); // 👈 return true if email exists in users array
     }
 }
