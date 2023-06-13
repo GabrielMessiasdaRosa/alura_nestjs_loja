facd4e8193208d62826c0fc2793cf2af343ce9d7
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-06-13 11:57:13 -0300

    feat: post users in memory

---
+++ src/user.service.ts
@@ -0,0 +1,13 @@
+export class UserService {
+    private users = []; // 👈 users array memory storage
+
+    async createUser(name: string, email: string, password: string) { // 👈 createUser method that accepts name, email, and password
+        const user = { // 👈 user object
+            name,
+            email,
+            password
+        };
+        this.users.push(user); // 👈 push user object to users array, this will be replaced with a database call in the next chapter
+        return user; // 👈 return user object
+    }
+}
