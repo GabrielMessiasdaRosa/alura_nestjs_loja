69244b98b1dc6d0fb5b94b6a328e91601f3c3696
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-06-14 12:34:16 -0300

    chore: add some important coments

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,12 @@ import { UserModule } from './user/user.module';
   imports: [
     UserModule,
     ProductModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
+    ConfigModule.forRoot({ // 👈 ConfigModule.forRoot() is a method that allows us to register the configuration file.
+      isGlobal: true, // 👈 isGlobal is a boolean that defines whether the configuration file should be available globally.
     }),
-    TypeOrmModule.forRootAsync({
-      useClass: PostgresConfigService,
-      inject: [PostgresConfigService],
+    TypeOrmModule.forRootAsync({ // 👈 TypeOrmModule.forRootAsync() is a method that allows us to register a dynamic configuration file.
+      useClass: PostgresConfigService, // 👈 useClass is a property that defines the class that implements TypeOrmOptionsFactory.
+      inject: [PostgresConfigService], // 👈 inject is a property that defines the dependencies that should be injected into the class that implements TypeOrmOptionsFactory.
     }),
   ],
 })
--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
-@Injectable()
-export class PostgresConfigService implements TypeOrmOptionsFactory {
-  constructor(private configService: ConfigService) {}
+@Injectable() // 👈 this is required
+export class PostgresConfigService implements TypeOrmOptionsFactory { // 👈 TypeOrmOptionsFactory is an interface that defines a method for creating TypeOrmModuleOptions.
+  constructor(private configService: ConfigService) {} // 👈 ConfigService is a service provided by the @nestjs/config package that allows us to access environment variables.
 
-  createTypeOrmOptions(): TypeOrmModuleOptions {
+  createTypeOrmOptions(): TypeOrmModuleOptions { // 👈 createTypeOrmOptions() is a method that returns TypeOrmModuleOptions.
     return {
-      type: 'postgres',
-      host: this.configService.get<string>('DB_HOST'),
-      port: this.configService.get<number>('DB_PORT'),
-      username: this.configService.get<string>('DB_USERNAME'),
-      password: this.configService.get<string>('DB_PASSWORD'),
-      database: this.configService.get<string>('DB_NAME'),
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
+      type: 'postgres', // 👈 type is the type of the database.
+      host: this.configService.get<string>('DB_HOST'), // 👈 host is the host of the database.
+      port: this.configService.get<number>('DB_PORT'), // 👈 port is the port of the database.
+      username: this.configService.get<string>('DB_USERNAME'), // 👈 username is the username of the database.
+      password: this.configService.get<string>('DB_PASSWORD'), // 👈 password is the password of the database.
+      database: this.configService.get<string>('DB_NAME'), // 👈 database is the name of the database.
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'], // 👈 entities is an array of entities that are used by the database.
+      synchronize: true, // 👈 synchronize is a boolean that defines whether the database schema should be auto-generated based on the entities. This should be set to false in production.
     };
   }
 }
